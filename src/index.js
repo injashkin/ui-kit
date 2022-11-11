a4eb78b191b92282cc549ab0de73dc9fff4961f1
injashkin <injashkin@gmail.com>
2022-11-11 23:23:00 +0300

    Update imports

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-import './components/button/button.scss';
-import './components/icon/icon.scss';
-import './components/forms/forms.scss';
-import './scss/utilities/_shapes.scss';
-import './scss/utilities/_loading.scss';
 import './style.scss';
 
 function component(text) {
